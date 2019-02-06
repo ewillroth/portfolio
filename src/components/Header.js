30fc5853562f2874cb782cb477c2e6848a73902e
Eric Willroth <ewillroth@gmail.com>
2019-02-06 09:56:21 -0600

    add required component files

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+		return (
+			<Nav></Nav>
+		);
+}
+
+const Nav = styled.nav`
+	background: blue;
+	height: 10vh;
+	width: 100%;
+`
+
+
+export default Header;
